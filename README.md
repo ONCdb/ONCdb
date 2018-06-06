903add57460cf80d30d5b6cddf25943b99b43209
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-06-06 10:37:39 -0400

    Updated README with db info.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ONCdb
-The Orion Nebula Cluster Database
+The Orion Nebula Cluster Database is a cross-matched and curated collection of astrometry, photometry, spectra, and image data for over 10,000 sources in the ONC.
+
+The current release of this database can be accessed via the Web application at [onc.stsci.edu](https://onc.stsci.edu). 
+
+## Acknowledgements
+If you use this database in your work, please cite the [Zenodo record of the appropriate release]().
+
+This project was made possible by the generous allocation of the Director's Discretionary Research Fund at the Space Telescope Science Institute. The authors include:
+
+- Joe Filippazzo
+- Mario Gennaro 
+- Massimo Robberto
--- make_onc.py
+++ make_onc.py
@@ -41,14 +41,14 @@ def ONC_catalogs_to_database(ra=83.81775*q.deg, dec=-5.38788889*q.deg, radius=0.
     
     # Get the radius from the ONC center which includes all Robberto+2013 sources
     center = coord.SkyCoord(ra=ra, dec=dec, frame='icrs')
-    radec = coord.SkyCoord(ra=onc.catalog['ra'], dec=onc.catalog['dec'], unit=(q.deg, q.deg), frame='icrs')
+    radec = coord.SkyCoord(ra=onc.sources['ra'], dec=onc.sources['dec'], unit=(q.deg, q.deg), frame='icrs')
     onc_radius = np.max(radec.separation(center)).value*q.deg
     
     # Get 2MASS
     onc.Vizier_query('II/246/out', 'TMASS', ra, dec, onc_radius, group=False)
 
-    # Get GAIA (Only 19 sources! Placeholder for DR2!)
-    onc.Vizier_query('I/337/tgas', 'TGAS', ra, dec, onc_radius, ra_col='_RA.icrs', dec_col='_DE.icrs', group=False)
+    # Get GAIA DR2
+    onc.Vizier_query('I/345/gaia2', 'GAIA', ra, dec, onc_radius, ra_col='RA_ICRS', dec_col='DE_ICRS', group=False)
 
     # Get ALLWISE
     onc.Vizier_query('II/328/allwise', 'ALLWISE', ra, dec, onc_radius, group=False)
@@ -85,7 +85,7 @@ def generate_ONCdb(cat):
     db = astrodb.Database(path+'orion.db')
     
     # Load the source list
-    source_list = at.Table(cat.catalog.values, names=cat.catalog.columns)
+    source_list = at.Table(cat.sources.values, names=cat.sources.columns)
 
     # Rename some columns
     source_list.rename_column('flag', 'comments')
@@ -101,7 +101,7 @@ def generate_ONCdb(cat):
                  ['2013yCat..22070010R','Robb13','','VizieR Online Data Catalog: HST Treasury Program on the ONC'],\
                  ['2003yCat.2246....0C','Cutr03','','VizieR Online Data Catalog: 2MASS All-Sky Catalog of Point Sources'],\
                  ['2014yCat.2328....0C','Cutr13','','VizieR Online Data Catalog: AllWISE Data Release '],\
-                 ['2016A&A...595A...1G','Gaia16','','Gaia DR1'],\
+                 ['2018A&A..in.prep...','Gaia18','','Gaia DR2'],\
                  ['2016yCat.5149....0L','Luo_16','','The second data release (DR2) of the LAMOST regular survey'],\
                  ['2015ApJS..219...12A','Alam15','','The Eleventh and Twelfth Data Releases of the Sloan Digital Sky Survey: Final Data from SDSS-III']\
                 ], 'publications', clean_up=False)
@@ -130,24 +130,12 @@ def generate_ONCdb(cat):
         add_wpc2_data(db, cat.WFPC2)
     except:
         print('No WFPC2 data added')
-
-    # Add the 2MASS photometry
-    try:
-        add_2MASS_data(db, cat.TMASS)
-    except:
-        print('No 2MASS data added')
-
-    # Add the ALLWISE photometry
-    try:
-        add_WISE_data(db, cat.ALLWISE)
-    except:
-        print('No WISE data added')
-
-    # Add the GAIA photometry and parallaxes
-    try:
-        add_TGAS_data(db, cat.TGAS)
-    except:
-        print('No TGAS data added')
+        
+    db.add_data(cat.SDSS, table='photometry', bands=['SDSS.u','SDSS.g','SDSS.r','SDSS.i','SDSS.z'], rename_columns='SDSS', column_fill='SDSS', clean_up=False)
+    db.add_data(cat.TMASS, table='photometry', bands=['2MASS.J','2MASS.H','2MASS.Ks'], rename_columns='2MASS', column_fill='2MASS', clean_up=False)
+    db.add_data(cat.ALLWISE, table='photometry', bands=['WISE.W1','WISE.W2','WISE.W3','WISE.W4'], rename_columns='WISE', column_fill='WISE', clean_up=False)
+    db.add_data(cat.GAIA, table='parallaxes', rename_columns='GAIA', column_fill='GAIA', clean_up=False)
+    db.add_data(cat.GAIA, table='photometry', rename_columns='GAIA', column_fill='GAIA', clean_up=False)
 
     # Add the spectral types
     try:
@@ -160,12 +148,6 @@ def generate_ONCdb(cat):
         add_LAMOST_data(db, cat.LAMOST)
     except:
         print('No LAMOST data added')
-        
-    # Add SDSS photometry
-    try:
-        add_SDSS_data(db, cat.SDSS)
-    except:
-        print('No SDSS data added')
         
     return db
 
@@ -198,41 +180,6 @@ def add_LAMOST_data(db, cat):
     # Add the data
     db.query("pragma foreign_keys=OFF")
     db.add_data(lamo, table='spectral_types', clean_up=False)
-    db.query("pragma foreign_keys=ON")
-            
-    db.save()
-    
-def add_TGAS_data(db, cat):
-    """
-    Read in the GAIA photometry and astrometry
-    """
-    # Read in the data
-    gaia = at.Table.from_pandas(cat)
-    
-    # Rename some columns
-    gaia.rename_column('Epoch', 'epoch')
-    
-    # Add columns for telescope_id, instrument_id, system_id, and publication_shortname
-    gaia['publication_shortname'] = ['Gaia16']*len(gaia)
-    gaia['telescope_id'] = [4]*len(gaia)
-    gaia['instrument_id'] = [7]*len(gaia)
-    gaia['system_id'] = [1]*len(gaia)
-    
-    # Rename and make sure the magntiudes are floats
-    # gaia.rename_column('__Gmag_', 'magnitude')
-    # gaia.rename_column('e__Gmag_', 'magnitude_unc')
-    # gaia['band'] = ['G']*len(gaia)
-    # gaia['magnitude'] = at.Column(gaia['magnitude'], 'magnitude', dtype=float)
-    # gaia['magnitude_unc'] = at.Column(gaia['magnitude_unc'], 'magnitude_unc', dtype=float)
-    # db.query("pragma foreign_keys=OFF")
-    # db.add_data(gaia, table='photometry', clean_up=False)
-    # db.query("pragma foreign_keys=ON")
-    
-    # Add the parallaxes
-    gaia.rename_column('Plx', 'parallax')
-    gaia.rename_column('e_Plx', 'parallax_unc')
-    db.query("pragma foreign_keys=OFF")
-    db.add_data(gaia, table='parallaxes', clean_up=False)
     db.query("pragma foreign_keys=ON")
             
     db.save()
@@ -416,136 +363,46 @@ def add_wpc2_data(db, cat):#file=path+'raw_data/viz_wfpc2_with_IDs.tsv'):
             pass
             
     db.save()
-
-def add_SDSS_data(db, cat):
-    """
-    Read in the cross matched SDSS data
-    """
-    # Read in the data
-    sdss = at.Table.from_pandas(cat)
     
-    # Rename some columns
-    sdss.rename_column('ObsDate', 'epoch')
-    sdss.rename_column('flags', 'oflags')
-    sdss.rename_column('Q', 'flags')
-    sdss.rename_column('e_umag','umag_unc')
-    sdss.rename_column('e_gmag','gmag_unc')
-    sdss.rename_column('e_rmag','rmag_unc')
-    sdss.rename_column('e_imag','imag_unc')
-    sdss.rename_column('e_zmag','zmag_unc')
-    
-    # Add columns for telescope_id, instrument_id, system_id, and publication_shortname
-    sdss['publication_shortname'] = ['Alam15']*len(sdss)
-    sdss['telescope_id'] = [6]*len(sdss)
-    sdss['instrument_id'] = [9]*len(sdss)
-    sdss['system_id'] = [2]*len(sdss)
-    
-    # Add the data
-    db.add_data(sdss, table='photometry', bands=['umag','gmag','rmag','imag','zmag'], clean_up=False)
-    
-    # Change band names
-    for i,j in zip(['umag','gmag','rmag','imag','zmag'],['SDSS.u','SDSS.g','SDSS.r','SDSS.i','SDSS.z']):
-        db.modify("UPDATE photometry SET band='{}' WHERE band='{}'".format(j,i))
-            
-    db.save()
-
-def add_2MASS_data(db, cat):
-    """
-    Read in the cross matched 2MASS data
-    """
-    # Read in the data
-    tmass = at.Table.from_pandas(cat)
-    
-    # Rename some columns
-    tmass.rename_column('JD', 'epoch')
-    tmass.rename_column('Qflg', 'flags')
-    tmass.rename_column('e_Jmag','Jmag_unc')
-    tmass.rename_column('e_Hmag','Hmag_unc')
-    tmass.rename_column('e_Kmag','Kmag_unc')
-    
-    # Add columns for telescope_id, instrument_id, system_id, and publication_shortname
-    tmass['publication_shortname'] = ['Cutr03']*len(tmass)
-    tmass['telescope_id'] = [2]*len(tmass)
-    tmass['instrument_id'] = [5]*len(tmass)
-    tmass['system_id'] = [2]*len(tmass)
-    
-    # Add the data
-    db.add_data(tmass, table='photometry', bands=['Jmag','Hmag','Kmag'], clean_up=False)
-    
-    # Change band names
-    for i,j in zip(['Jmag','Hmag','Kmag'],['2MASS.J','2MASS.H','2MASS.Ks']):
-        db.modify("UPDATE photometry SET band='{}' WHERE band='{}'".format(j,i))
-        
-    db.save()
-
-def add_WISE_data(db, cat):
-    """
-    Read in the cross matched WISE data
-    """
-    # Read in the data
-    wise = at.Table.from_pandas(cat)
-    
-    # Rename some columns
-    wise.rename_column('qph', 'flags')
-    wise.rename_column('e_W1mag','W1mag_unc')
-    wise.rename_column('e_W2mag','W2mag_unc')
-    wise.rename_column('e_W3mag','W3mag_unc')
-    wise.rename_column('e_W4mag','W4mag_unc')
-    
-    # Add columns for telescope_id, instrument_id, system_id, and publication_shortname
-    wise['publication_shortname'] = ['Cutr13']*len(wise)
-    wise['telescope_id'] = [3]*len(wise)
-    wise['instrument_id'] = [6]*len(wise)
-    wise['system_id'] = [2]*len(wise)
-    
-    # Add the data
-    db.add_data(wise, table='photometry', bands=['W1mag','W2mag','W3mag','W4mag'], clean_up=False)
-    
-    # Change band names
-    for i,j in zip(['W1mag','W2mag','W3mag','W4mag'],['WISE.W1','WISE.W2','WISE.W3','WISE.W4']):
-        db.modify("UPDATE photometry SET band='{}' WHERE band='{}'".format(j,i))
-        
-    db.save()
-
 def add_Hill13_data(db, cat):
     """
     Read in the cross matched Hillenbrand+13 data
     """
     # Read in the data
     hill13 = at.Table.from_pandas(cat)
-    
+
     # Collect the spectral types
     spts = hill13['SpT2']
-    
+
     # Convert the spectral types to integers
     spts = [specType(s) if isinstance(s, str) else [np.nan,''] for s in spts]
     typ, lc = np.array(spts).T
-    
+
     # Add to the table
     hill13['spectral_type'] = typ
     hill13['luminosity_class'] = lc
-    
+
     # Add column publication_shortname
     hill13['publication_shortname'] = ['Hill13']*len(hill13)
-    
+
     # Add the data
     db.query("pragma foreign_keys=OFF")
     db.add_data(hill13, table='spectral_types', clean_up=False)
     db.query("pragma foreign_keys=ON")
-    
+
     db.save()
 
 def specType(SpT, types=[i for i in 'OBAFGKMLTY'], verbose=False):
     """
     Converts between float and letter/number spectral types (e.g. 14.5 => 'B4.5' and 'A3' => 23).
-    
+
     Parameters
     ----------
     SpT: float, str
         Float spectral type or letter/number spectral type between O0.0 and Y9.9
     types: list
         The MK spectral type letters to include, e.g. ['M','L','T','Y']
-      
+  
     Returns
     -------
     list, str
@@ -564,12 +421,12 @@ def specType(SpT, types=[i for i in 'OBAFGKMLTY'], verbose=False):
                              .replace('-1','').replace(':','').replace('?','')\
                              .replace('-V','').replace('p','').replace('<','')\
                              .replace('>','')
-                
+            
                 if suf.replace('.','').isdigit():
                     val = float(suf)
-                    
-                else:
                 
+                else:
+            
                     for cl in ['III','V','IV']:
                         try:
                             idx = suf.find(cl)
@@ -581,7 +438,7 @@ def specType(SpT, types=[i for i in 'OBAFGKMLTY'], verbose=False):
                                 val = float(suf)
                             except:
                                 continue
-                                
+                            
                 # return [types.index(MK)*10+val-(4. if MK in ['M','L','T','Y'] else 0), LC]
                 return [types.index(MK)*10+val, LC]
 
@@ -595,8 +452,8 @@ def specType(SpT, types=[i for i in 'OBAFGKMLTY'], verbose=False):
         else:
             if verbose:
                 print('Sir, Spectral type',SpT,'must be a float between 0 and',len(types)*10,'or a string of class',types)
-        
+    
     except:
         pass
-        
+    
     return result
